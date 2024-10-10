a6badcca00a01faf859dbc020775327d1801e020
Owen Gometz <owengometz@gmail.com>
2024-10-10 10:14:55 -0400

    in class activity multiplication addition and largest

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -16,5 +16,31 @@ export default function QueryProcessor(query: string): string {
       "Owen Gometz"
     );
   }
+  if (query.toLowerCase().includes("name")) {
+    return (
+      "Owen Gometz"
+    );
+  }
+  const additionMatch = query.match(/what is (\d+) plus (\d+)/i);
+  if (additionMatch) {
+    const num1 = parseInt(additionMatch[1], 10);
+    const num2 = parseInt(additionMatch[2], 10);
+    return `${num1 + num2}`;
+  }
+  const largestMatch = query.match(/which of the following numbers is the largest: (\d+), (\d+), (\d+)/i);
+    if (largestMatch) {
+      const num1 = parseInt(largestMatch[1], 10);
+      const num2 = parseInt(largestMatch[2], 10);
+      const num3 = parseInt(largestMatch[3], 10);
+      const largestNumber = Math.max(num1, num2, num3);
+      return `${largestNumber}`;
+    }
+  const multipluyMatch = query.match(/what is (\d+) multiplied by (\d+)/i);
+  if (multipluyMatch) {
+    const num1 = parseInt(multipluyMatch[1], 10);
+    const num2 = parseInt(multipluyMatch[2], 10);
+    return `${num1 * num2}`;
+  }
+
   return "";
 }
